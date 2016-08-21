dfb6837e8b99abe84be8b97ee211421be3ad2e18
Rezwan Rony <rezwanronyrony@gmail.com>
2016-08-21 15:24:58 +0600

    inital

---
+++ app/src/main/java/com/appracks/GhostStories/MainActivity.java
@@ -0,0 +1,78 @@
+package com.appracks.GhostStories;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout myDrawer;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    ImageView img_bio,img_cover,img_quotes,img_videos,img_gallery,img_lifestyle;
+    TextView tv_bio,tv_cover,tv_quotes,tv_videos,tv_gallery,tv_lifestyle,tv_aboutmessi,tv_achievements,tv_connectmessi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=(Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        myDrawer=(DrawerLayout)findViewById(R.id.myDrawer);
+        setNavMenu();
+
+    }
+
+    private void setNavMenu() {
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this, myDrawer,toolbar, R.string.drawer_open, R.string.drawer_close);
+        //noinspection deprecation
+        myDrawer.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
+        alertDialog.setTitle("Are you sure to want to quit?");
+        alertDialog.setCancelable(false);
+        alertDialog.setIcon(R.mipmap.ic_launcher);
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+                System.exit(0);
+            }
+        });
+        alertDialog.setNegativeButton("No", null);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
